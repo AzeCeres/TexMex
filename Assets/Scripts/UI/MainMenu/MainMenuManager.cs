b77987fd258a6b6d3759d266acb83048c2e9625c
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2022-04-21 11:29:02 +0200

    Playerprefs, settingscontroller scrub

--- Assets/Scripts/UI/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/UI/MainMenu/MainMenuManager.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
-
+    public SettingsController settingsController;
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject optionsMenu;
     // Start is called before the first frame update
     void Start()
     {
         MainMenu();
+        GetPlayerPrefs();
     }
 
     public void PlayGame()
@@ -29,6 +30,16 @@ public class MainMenuManager : MonoBehaviour
     {
         mainMenu.SetActive(true);
         optionsMenu.SetActive(false);
+    }
+
+    private void GetPlayerPrefs()
+    {
+        if (!PlayerPrefs.HasKey("MasterVolume")) return;
+        print("get player prefs");
+        settingsController.masterVolume = PlayerPrefs.GetFloat("MasterVolume");
+        settingsController.musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        settingsController.environmentVolume = PlayerPrefs.GetFloat("EnvironmentVolume");
+        settingsController.playerVolume = PlayerPrefs.GetFloat("PlayerVolume");
     }
 
     public void QuitGame()
--- Assets/Scripts/UI/MainMenu/OptionsMenu.cs
+++ Assets/Scripts/UI/MainMenu/OptionsMenu.cs
@@ -2,17 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
 
+    public SettingsController settingsController;
+
     [SerializeField] private GameObject[] optionsMenus;
+    [SerializeField] private Slider[] volumeSliders;
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     private void OnEnable()
     {
@@ -37,5 +37,47 @@ public class OptionsMenu : MonoBehaviour
             i.SetActive(false);
         }
         optionsMenus[1].SetActive(true);
+        SaveVolume();
+        SetSliderValue();
+    }
+
+    public void SetVolume()
+    {
+        settingsController.masterVolume = volumeSliders[0].value;
+        settingsController.musicVolume = volumeSliders[1].value;
+        settingsController.environmentVolume = volumeSliders[2].value;
+        settingsController.playerVolume = volumeSliders[3].value;
+    }
+
+    private void SetSliderValue()
+    {
+        volumeSliders[0].value = PlayerPrefs.GetFloat("MasterVolume");
+        volumeSliders[1].value = PlayerPrefs.GetFloat("MusicVolume");
+        volumeSliders[2].value = PlayerPrefs.GetFloat("EnvironmentVolume");
+        volumeSliders[3].value = PlayerPrefs.GetFloat("PlayerVolume");
+    }
+
+    public void SaveVolume()
+    {
+        PlayerPrefs.SetFloat("MasterVolume", settingsController.masterVolume);
+        PlayerPrefs.SetFloat("MusicVolume", settingsController.musicVolume);
+        PlayerPrefs.SetFloat("EnvironmentVolume", settingsController.environmentVolume);
+        PlayerPrefs.SetFloat("PlayerVolume", settingsController.playerVolume);
+    }
+    
+    public void VolumeBackButton()
+    {
+        if (!PlayerPrefs.HasKey("MasterVolume"))
+        {
+            settingsController.masterVolume = 100;
+            settingsController.musicVolume = 100;
+            settingsController.environmentVolume = 100;
+            settingsController.playerVolume = 100;
+            return;
+        }
+        settingsController.masterVolume = PlayerPrefs.GetFloat("MasterVolume");
+        settingsController.musicVolume = PlayerPrefs.GetFloat("MusicVolume");
+        settingsController.environmentVolume = PlayerPrefs.GetFloat("EnvironmentVolume");
+        settingsController.playerVolume = PlayerPrefs.GetFloat("PlayerVolume");
     }
 }
