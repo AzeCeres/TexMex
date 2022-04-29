c069a30ff1dce2be03e9576b71b6301de30a7e42
Woolrich221 <ulrikschaeffer2002@gmail.com>
2022-04-29 18:11:03 +0200

    forg

--- Assets/Scripts/Puzzle/Frogpot.cs
+++ Assets/Scripts/Puzzle/Frogpot.cs
@@ -1,18 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Frogpot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    private bool broken = false;
+    [SerializeField] private bool breaking = false;
+    [SerializeField] private GameObject forg;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other)
     {
+        if (broken = false);
+        {
+            breaking = true;
+        }
+        if (breaking)
+        {
+            animator.Play("ForgPot_broKen");
+            broken = true;
+            Instantiate(forg, new Vector3(0, 0, 0), Quaternion.identity);
+            breaking = false;
+        }
         
     }
 }
